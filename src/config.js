60a40be53a4e6a12ce8c89895f6bae9300b045dc
Lucas Bruno <lucasbrunoferreira@gmail.com>
2018-02-04 22:04:00 -0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyByMkFa0RS4vbJOcO-ljmUst7ohbjx1dtg',
-  authDomain: 'tech-store-80b74.firebaseapp.com',
-  databaseURL: 'https://tech-store-80b74.firebaseio.com',
-  projectId: 'tech-store-80b74',
-  storageBucket: 'tech-store-80b74.appspot.com',
-  messagingSenderId: '37861070063',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
 });
 
 export const db = firebaseApp.database();
